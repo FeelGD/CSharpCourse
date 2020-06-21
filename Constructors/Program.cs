4f3e834c7c32be59d9cd32e05c7ce684430b376f
Ali İhsan YILDIZ <46348313+Khonichiva@users.noreply.github.com>
2020-06-21 23:31:20 +0300

    v2
    Constructors uses 2

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,15 +15,24 @@ namespace Constructors
             CustomerManager customerManager = new CustomerManager();
             customerManager.List();
 
+
+            Product product = new Product { Id = 1, Name = "Laptop" };
+            Product product2 =new Product();
+            Product product3 = new Product(2,"Laptop");
+
+
             Console.ReadLine();
         }
     }
+
+
 
     class CustomerManager
     {
         //sınıfın ihtiyaç duyduğu parametreler değişkenlik gösteriyorsa.
         //private bir field "_" ile başlatılır.
-        private int _count=15;
+        private int _count = 15;
+
         public CustomerManager(int count)
         {
             _count = count;
@@ -30,7 +40,7 @@ namespace Constructors
 
         public CustomerManager()
         {
-            
+
         }
         //constructors overloading 
 
@@ -41,7 +51,7 @@ namespace Constructors
 
         public void List()
         {
-            Console.WriteLine("Listed {0} items" ,_count);
+            Console.WriteLine("Listed {0} items", _count);
         }
 
         public void Add()
@@ -49,5 +59,25 @@ namespace Constructors
             Console.WriteLine("Added {0} items", _count);
 
         }
+
+
+    }
+
+    class Product
+    {
+        public Product()
+        {
+            
+        }
+
+        private int _id;
+        private string _name;
+        public Product(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
