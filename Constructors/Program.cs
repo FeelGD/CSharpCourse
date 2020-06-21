bf350a831fc3cc400ee55fd395a8e2b9a2531aae
Ali İhsan YILDIZ <46348313+Khonichiva@users.noreply.github.com>
2020-06-21 23:25:49 +0300

    v1
    basic Constructors using

--- Constructors/Program.cs
+++ Constructors/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace Constructors
 {
+    //bir sınıf newlendiğinde çalışacak kod dilimidir.
     class Program
     {
         static void Main(string[] args)
         {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.List();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        //sınıfın ihtiyaç duyduğu parametreler değişkenlik gösteriyorsa.
+        //private bir field "_" ile başlatılır.
+        private int _count=15;
+        public CustomerManager(int count)
+        {
+            _count = count;
+        }
+
+        public CustomerManager()
+        {
+            
+        }
+        //constructors overloading 
+
+
+
+
+
+
+        public void List()
+        {
+            Console.WriteLine("Listed {0} items" ,_count);
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Added {0} items", _count);
+
         }
     }
 }
