345fb1a45a217d05d9aaaa6e43211229c6ba428d
Ali İhsan YILDIZ <46348313+Khonichiva@users.noreply.github.com>
2020-06-24 00:08:08 +0300

    v1
    Generics 101

---
+++ Generics/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+    class Product
+    {
+        
+    }
+    interface IProductDal:IRepository<Product>
+    {
+        
+    }
+
+    class Customer
+    {
+        
+    }
+    interface ICustomerDal:IRepository<Customer>
+    {
+        void Custom();
+
+    }
+    //sıklıkla yaptığımız operasyonları nesne tarzında değiştirebiliriz.
+    interface IRepository<T>//T=type
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+
+    class ProductDal:IProductDal
+    {
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomerDal:ICustomerDal
+    {
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+ 
